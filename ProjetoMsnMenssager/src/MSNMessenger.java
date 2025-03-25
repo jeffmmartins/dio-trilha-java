af971c5700266c8961636b2dfe31bf109d1ad2ef
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-03-25 17:56:26 -0300

    classe msnmessenger criada

---
+++ ProjetoMsnMenssager/src/MSNMessenger.java
@@ -0,0 +1,21 @@
+import javax.sound.midi.Soundbank;
+
+public class MSNMessenger {
+    public void enviarMensagem(){
+        System.out.println("Enviado mensagem");
+    }
+
+    public void receberMensage(){
+        System.out.println("Recebendo mensageem");
+    }
+
+    public void validarConectandoInternet(){
+        System.out.println("Conectando Internet");
+    }
+
+    public void salvarHistoricoMensagem(){
+        System.out.println("Salvando Mensagem");
+    }
+
+
+}
