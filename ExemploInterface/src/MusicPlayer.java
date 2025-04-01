4687618e825c43a055445167f06cbe5cde043ec8
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-04-01 18:27:25 -0300

    exemplo de interface

---
+++ ExemploInterface/src/MusicPlayer.java
@@ -0,0 +1,12 @@
+public interface MusicPlayer {
+
+   // mesma jeito de public final static String music;
+    String music = "Parabéns para voce";
+
+    // metodo abstrato;
+    void playMusic();
+
+    void pauseMusic();
+
+    void stopMusic();
+}
