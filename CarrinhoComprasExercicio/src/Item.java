206be02598c1b7200c5d92d1520c5a22c0a216c6
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-04-05 20:55:02 -0300

    classe item finalizada

--- CarrinhoComprasExercicio/src/Item.java
+++ CarrinhoComprasExercicio/src/Item.java
@@ -1,2 +1,28 @@
 public class Item {
+    private String nome;
+    private String preco;
+    private String quaantidade;
+
+    // construtor padrão;
+    public Item(){
+
+    }
+
+    public Item(String nome, String preco, String quaantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quaantidade = quaantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getPreco() {
+        return preco;
+    }
+
+    public String getQuaantidade() {
+        return quaantidade;
+    }
 }
