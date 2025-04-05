f30311af8af6776a45bb117fdf8389ba4c439145
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-04-05 18:24:10 -0300

    classe Lista tarefa finaliza

--- List/src/ListaTarefa.java
+++ List/src/ListaTarefa.java
@@ -1,3 +1,36 @@
-public class ListaTarefa {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ListaTarefa {
+     // atributo
+    private List<Tarefas> tarefaList;
+
+    // construtor, sempre que criar objeto listatarefa vai ter um array
+    // list vazio.
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao) {
+        tarefaList.add(new Tarefas(descricao));
+    }
+
+    public void removerTarefa(String descricao){
+        List<Tarefas> tarefasParaRemover = new ArrayList<>();
+        for (Tarefas t : tarefaList){
+            if(t.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefasParaRemover.add(t);
+            }
+            tarefaList.removeAll(tarefasParaRemover);
+        }
+    }
+
+    public int obterNumeroTarefas () {
+        // retorna o tamanho do list
+        return tarefaList.size();
+    }
+
+    public void obterDescricaoTarefa(){
+        System.out.println(tarefaList);
+    }
 }
