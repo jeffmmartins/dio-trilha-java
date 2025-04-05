72d0d44616b83401b652d7bdfb8a159120e91525
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-04-05 17:54:07 -0300

    inicializando a classe lista tarefa

--- List/src/Tarefas.java
+++ List/src/Tarefas.java
@@ -1,2 +1,19 @@
 public class Tarefas {
+    // atribuição
+    private String descricao;
+
+
+    //construtor padrão
+    public Tarefas(){
+
+    }
+
+    // construtor
+    public Tarefas(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
 }
