62ade08989d8e4e3bac77f8a46bf837dbd88ee3f
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-04-07 08:47:53 -0300

    classe adicionar pessoa finalizada

--- OrdenacaoList/src/OrdenacaoPessoa.java
+++ OrdenacaoList/src/OrdenacaoPessoa.java
@@ -1,3 +1,16 @@
-public class OrdenacaoPessoa {
+import java.util.ArrayList;
+import java.util.List;
 
+public class OrdenacaoPessoa {
+    // atributo
+    List<Pessoa> listpessoa;
+
+    //construtor
+    public OrdenacaoPessoa() {
+        this.listpessoa = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura){
+        listpessoa.add(new Pessoa(nome, idade, altura));
+    }
 }
