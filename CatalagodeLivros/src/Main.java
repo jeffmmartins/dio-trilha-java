acefb8080ab93af9257c7597290b78bec74dd64e
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-04-07 08:07:46 -0300

    instanciando objeto de livros

--- CatalagodeLivros/src/Main.java
+++ CatalagodeLivros/src/Main.java
@@ -2,14 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+            CatalagoDeLivros catalagoDeLivros = new CatalagoDeLivros();
+
+            catalagoDeLivros.adicionarLivro("livro1", "Autor 1",2020 );
+            catalagoDeLivros.adicionarLivro("livro2", "Autor 2",2021 );
+            catalagoDeLivros.adicionarLivro("livro3", "Autor 3",2022 );
+            catalagoDeLivros.adicionarLivro("livro4", "Autor 4",2023 );
+            catalagoDeLivros.adicionarLivro("livro5", "Autor 5",1994 );
     }
 }
