1ff0db111e8a1a058dd026242b45d3d71f77a2e6
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-04-08 08:27:12 -0300

    classe convite finalizada

--- ExemploSet/src/Convidado.java
+++ ExemploSet/src/Convidado.java
@@ -1,2 +1,26 @@
 public class Convidado {
+    //atributos
+    private String nome;
+    private int convite;
+
+    public Convidado(String nome, int convite) {
+        this.nome = nome;
+        this.convite = convite;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getConvite() {
+        return convite;
+    }
+
+    @Override
+    public String toString() {
+        return "Convidado{" +
+                "nome='" + nome + '\'' +
+                ", convite=" + convite +
+                '}';
+    }
 }
