bae208d9e06b13077be2d2d8fb768e530afdbe90
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-04-08 07:56:28 -0300

    metodo ordenação por idade finalizado

--- OrdenacaoList/src/OrdenacaoPessoa.java
+++ OrdenacaoList/src/OrdenacaoPessoa.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class OrdenacaoPessoa {
@@ -12,5 +14,13 @@ public class OrdenacaoPessoa {
 
     public void adicionarPessoa(String nome, int idade, double altura){
         listpessoa.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade(){
+        //list não está vazia pois precisa ordenar por idade, por isso está duplicando
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(listpessoa);
+        //Ordenar as pessoas por idade atraves do comparable.
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
     }
 }
