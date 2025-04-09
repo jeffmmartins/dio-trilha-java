62ec5b00607494595588ded09fdf3afc7c5f480c
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-04-09 08:47:48 -0300

    classe conjunto palavras unicas finalizada

--- ExercicioConjuntoPalavrasUnicas/src/ConjuntoPalavrasUnicas.java
+++ ExercicioConjuntoPalavrasUnicas/src/ConjuntoPalavrasUnicas.java
@@ -1,2 +1,40 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class ConjuntoPalavrasUnicas {
+    // atributos
+    private Set<String> conjuntoPalavrasUnicasSet;
+
+    public ConjuntoPalavrasUnicas() {
+        this.conjuntoPalavrasUnicasSet = new HashSet<>();
+    }
+
+
+    public void adicionarPalavra(String palavra){
+        conjuntoPalavrasUnicasSet.add(palavra);
+    }
+
+    public void removerPalavra(String palavra){
+        if (!conjuntoPalavrasUnicasSet.isEmpty()){
+            if (conjuntoPalavrasUnicasSet.contains(palavra)){
+                conjuntoPalavrasUnicasSet.remove(palavra);
+            }else {
+                System.out.println("Palavra não encontrada no conjunto");
+            }else {
+                System.out.println("Conjunto vazio");
+            }
+        }
+    }
+
+    public boolean verificarPalavra(String palavra){
+        return conjuntoPalavrasUnicasSet.contains(palavra);
+    }
+
+    public void exibirPalavrasUnicas(){
+        if (!conjuntoPalavrasUnicasSet.isEmpty()){
+            System.out.println(conjuntoPalavrasUnicasSet);
+        }else {
+            System.out.println("Conjunto está vazio");
+        }
+    }
 }
