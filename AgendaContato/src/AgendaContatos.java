876a9beb9f2634d91e2da57348941a1058cdbdc8
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-04-11 23:25:53 -0300

    metodo adicionar contato criado

--- AgendaContato/src/AgendaContatos.java
+++ AgendaContato/src/AgendaContatos.java
@@ -1,6 +1,16 @@
+import java.util.HashSet;
 import java.util.Set;
 
 public class AgendaContatos {
     //atributo
     private Set<Contato> contato;
+
+    // construtor
+    public AgendaContatos() {
+        this.contato = new HashSet<>();
+    }
+
+    public void adicionarContato(String nome, int numero){
+        contato.add(new Contato(nome, numero));
+    }
 }
