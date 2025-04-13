605ada612883f8d9bdc0b0e50eb3b115b50dcde1
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-04-13 19:28:59 -0300

    testes finalizados

--- AgendaContato/src/Main.java
+++ AgendaContato/src/Main.java
@@ -2,14 +2,21 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.exibirContato();
+
+        agendaContatos.adicionarContato("camila", 123456);
+        agendaContatos.adicionarContato("camila", 0);
+        agendaContatos.adicionarContato("camila cavaclcante", 11111111);
+        agendaContatos.adicionarContato("camila dio", 654987);
+        agendaContatos.adicionarContato("maria dio", 654987);
+
+        agendaContatos.exibirContato();
+
+        System.out.println("Contato" + agendaContatos.pesquisarPorNome("camila"));
+
+        System.out.println("Contato atualizada" + agendaContatos.atualizarContato("maria dio", 22222));
     }
 }
