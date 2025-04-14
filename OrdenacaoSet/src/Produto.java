c393e7d5288645691b3da1dfd802e38d83bc4ed9
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-04-14 07:41:45 -0300

    classe pproduto finalizada

--- OrdenacaoSet/src/Produto.java
+++ OrdenacaoSet/src/Produto.java
@@ -1,3 +1,55 @@
-public class Produto {
+import java.util.Objects;
 
+public class Produto {
+    //atributos
+    private long codigo;
+    private String nome;
+    private Double preco;
+    private int quantidade;
+
+    public Produto(long codigo, String nome, Double preco, int quantidade) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return getCodigo() == produto.getCodigo();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getCodigo());
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "codigo=" + codigo +
+                ", nome='" + nome + '\'' +
+                ", preco=" + preco +
+                ", quantidade=" + quantidade +
+                '}';
+    }
 }
