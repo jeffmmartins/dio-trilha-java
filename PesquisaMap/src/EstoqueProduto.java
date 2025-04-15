981fe653c5aff0a36cc7ca011e30be5044bcb8b8
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-04-15 08:40:47 -0300

    metodo adicionar produto finalizado

--- PesquisaMap/src/EstoqueProduto.java
+++ PesquisaMap/src/EstoqueProduto.java
@@ -1,2 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class EstoqueProduto {
+    //atributos, codigo do tipo long;.
+    private Map<Long,Produto> estoqueProdutoMap;
+
+    public EstoqueProduto() {
+        this.estoqueProdutoMap = new HashMap<>();
+    }
+
+    public void adicionarProduto(long cod, String nome, double preco, int quantidade){
+        estoqueProdutoMap.put(cod, new Produto(nome,preco,quantidade));
+    }
 }
