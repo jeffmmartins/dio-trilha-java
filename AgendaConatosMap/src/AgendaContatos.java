641eea467228d08b38b3ac8ca564ad3b3be299e7
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-04-15 07:49:29 -0300

    calsse agenda contatos finalizada

--- AgendaConatosMap/src/AgendaContatos.java
+++ AgendaConatosMap/src/AgendaContatos.java
@@ -1,2 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class AgendaContatos {
+    //atributos
+    private Map<String, Integer> agendaContatoMap;
+
+    public AgendaContatos() {
+        this.agendaContatoMap = new HashMap<>();
+    }
+
+    public void adicionarContato(String nome, Integer telefone){
+        agendaContatoMap.put(nome, telefone);
+    }
+
+    public void removerContato(String nome){
+        if (!agendaContatoMap.isEmpty()){
+            agendaContatoMap.remove(nome);
+        }
+    }
+
+    public void exibirContatos(){
+        System.out.println(agendaContatoMap);
+    }
+
+    public Integer pesquisarPorNome(String nome){
+        Integer telefonePorNome = null;
+        if (!agendaContatoMap.isEmpty()){
+            telefonePorNome = agendaContatoMap.get(nome);
+        }
+        return telefonePorNome;
+    }
 }
