2d4ed60aeb820096913a57b94a2883694647b84a
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-04-15 08:17:19 -0300

    testes finalizados

--- AgendaConatosMap/src/Main.java
+++ AgendaConatosMap/src/Main.java
@@ -2,14 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.adicionarContato("camila", 123456);
+        agendaContatos.adicionarContato("camila", 5665);
+        agendaContatos.adicionarContato("camila cavalcante", 1111111);
+        agendaContatos.adicionarContato("camila dio", 654987);
+        agendaContatos.adicionarContato("maria silva", 1111111);
+        agendaContatos.adicionarContato("camila", 4444);
+
+        agendaContatos.exibirContatos();
+        agendaContatos.removerContato("maria silva");
+        agendaContatos.exibirContatos();
+
     }
 }
