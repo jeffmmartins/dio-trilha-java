a441573d076b1f0aa2f257e55d753334b0539dd4
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-04-16 07:45:19 -0300

    testes finalizados

--- PesquisaMap/src/Main.java
+++ PesquisaMap/src/Main.java
@@ -2,6 +2,26 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        EstoqueProduto estoque = new EstoqueProduto();
 
+        // Exibe o estoque vazio
+        estoque.exibirProdutos();
+
+        // Adiciona produtos ao estoque
+        estoque.adicionarProduto(1L, "Notebook", 1500.0,   1);
+        estoque.adicionarProduto(2L, "Mouse", 25.0, 5);
+        estoque.adicionarProduto(3L, "Monitor", 400.00, 10);
+        estoque.adicionarProduto(4L, "Teclado", 40.0, 2);
+
+        // Exibe os produtos no estoque
+        estoque.exibirProdutos();
+
+        // Calcula e exibe o valor total do estoque
+        System.out.println("Valor total do estoque: R$" + estoque.calcularValorTotalEstoque());
+
+        // Obtém e exibe o produto mais caro
+        Produto produtoMaisCaro = estoque.obterProdutoMaisCaro();
+        System.out.println("Produto mais caro: " + produtoMaisCaro);
     }
+
 }
