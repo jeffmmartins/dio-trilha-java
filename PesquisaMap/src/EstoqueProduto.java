4c03421e270d7a5f694c8e4455d9eb462aeac4f5
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-04-16 07:30:03 -0300

    classe estoqueProduto finalizado

--- PesquisaMap/src/EstoqueProduto.java
+++ PesquisaMap/src/EstoqueProduto.java
@@ -1,3 +1,4 @@
+import java.security.PublicKey;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,5 +12,30 @@ public class EstoqueProduto {
 
     public void adicionarProduto(long cod, String nome, double preco, int quantidade){
         estoqueProdutoMap.put(cod, new Produto(nome,preco,quantidade));
+    }
+
+    public void exibirProdutos(){
+        System.out.println(estoqueProdutoMap);
+    }
+
+    public double calcularValorTotalEstoque(){
+        double valorTotalEstoque = 0d;
+        if(!estoqueProdutoMap.isEmpty()){
+            for (Produto p : estoqueProdutoMap.values()){
+                valorTotalEstoque += p.getQuantidade() * p.getPreco();
+            }
+        }
+        return valorTotalEstoque;
+    }
+
+    public Produto obterProdutoMaisCaro(){
+        Produto produtoMaisCaro = null;
+        double maiorPreco = Double.MIN_VALUE;
+        for (Produto p : estoqueProdutoMap.values()){
+            if (p.getPreco() > maiorPreco){
+                produtoMaisCaro = p;
+            }
+        }
+        return produtoMaisCaro;
     }
 }
