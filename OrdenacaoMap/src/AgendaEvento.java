8e93cf68ddf77cad5be83f59d69d8dfbdb41456f
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-04-16 08:36:46 -0300

    metodo exibir agenda finalizado

--- OrdenacaoMap/src/AgendaEvento.java
+++ OrdenacaoMap/src/AgendaEvento.java
@@ -1,2 +1,28 @@
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class AgendaEvento {
+    // atributo
+    private Map<LocalDate, Evento> eventoMap;
+
+    public AgendaEvento() {
+        this.eventoMap = new HashMap<>();
+    }
+
+    public void adicionarEvento (LocalDate data, String nome, String atracao){
+        // intanciado Evento
+        Evento evento = new Evento(nome,atracao);
+        eventoMap.put(data,evento);
+    }
+
+    public void exibirAgenda(){
+        Map<LocalDate, Evento> eventoTreeMap = new TreeMap<>(eventoMap);
+        System.out.println(eventoTreeMap);
+    }
+
+    public void obterProximoEvento() {
+
+    }
 }
