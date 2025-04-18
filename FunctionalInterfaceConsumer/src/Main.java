1f2a334e69e87758d608192fe1eaf0ef278bdc61
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-04-18 18:46:23 -0300

    implementando o consumer

---
+++ FunctionalInterfaceConsumer/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1,2,3,4,5,6);
+
+        /*Consumer<Integer> numerosPar = numero -> {
+            if (numero % 2 == 0){
+                System.out.println(numero);
+            }
+        };*/
+        //impplementando o consumer
+        numeros.stream().forEach(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer n) {
+                if (n % 2 == 0){
+                    System.out.println(n);
+                }
+            }
+        });
+    }
+}
