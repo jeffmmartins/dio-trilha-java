abc2b861f4e4e4a3113417a946336fb92bb4ef5f
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-04-18 19:09:40 -0300

    exemplo de supplier finalizado

---
+++ FunctionalInterfaceConsumer/src/FuncTionalInterface/suplier.java
@@ -0,0 +1,19 @@
+package FuncTionalInterface;
+
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class suplier {
+    public static void main(String[] args) {
+        Supplier<String> saudacao = () -> "Seja bem vindo";
+
+        //Usar o supplier para obter a lista
+        List<String> listaSaudações = Stream.generate(saudacao)
+                .limit(5)
+                .collect(Collectors.toList());
+
+        listaSaudações.forEach(System.out::println);
+    }
+}
