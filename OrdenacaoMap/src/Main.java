509b418d66244f1da6faac8889af26e246e14c72
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-04-18 11:15:26 -0300

    teste finalizado

--- OrdenacaoMap/src/Main.java
+++ OrdenacaoMap/src/Main.java
@@ -1,8 +1,13 @@
+import java.time.LocalDate;
+import java.time.Month;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         AgendaEvento agendaEvento = new AgendaEvento();
 
+
+        agendaEvento.adicionarEvento(LocalDate.of(2022, Month.JULY, 15), evento1, atracao1);
     }
 }
