06992c33dbf1d9f65d89d5c3843a7f9f82dc22c7
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-04-19 07:07:12 -0300

    teste finalizado

--- Switch/src/Main.java
+++ Switch/src/Main.java
@@ -2,14 +2,18 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        AutoPolicy autoPolicy1 = new AutoPolicy(11111111, "Toyota Camry", "NJ");
+        AutoPolicy autoPolicy2 = new AutoPolicy(22222222, "Ford Fusion", "MF");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        policyInNoFaultState(autoPolicy1);
+        policyInNoFaultState(autoPolicy2);
+    }
+
+    public static void policyInNoFaultState (AutoPolicy policy){
+        System.out.println("The auto policy");
+        System.out.printf("Account #: %d; Car: %s;  State: %s %s a no-fault state%n%n",
+                policy.getAccountNumber(),policy.getMakeANdModel(),
+                policy.getSatate(),
+                (policy.isNoFaultState() ? "is" : "is not"));
     }
 }
