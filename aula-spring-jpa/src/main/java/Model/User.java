b9acc839afa11032a285d3ae2affd9e3d81ea015
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-04-23 09:00:50 -0300

    atributos adicionados

--- aula-spring-jpa/src/main/java/Model/User.java
+++ aula-spring-jpa/src/main/java/Model/User.java
@@ -1,4 +1,40 @@
 package Model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class User {
+    @Id
+    // id gerenciado de forma automatica no banco, com estrutura de identificação.
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Integer id;
+    private String name;
+    private String username;
+    private String password;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
