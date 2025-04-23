e173a39118d99d266e94505e412c92690a0f9024
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-04-23 08:51:25 -0300

    inicializando a classe user

---
+++ aula-spring-jpa/src/main/java/dio_aula_jpa/AulaSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package dio_aula_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaSpringJpaApplication.class, args);
+	}
+
+}
---
+++ aula-spring-jpa/src/test/java/dio_aula_jpa/AulaSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package dio_aula_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaSpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
