309398ed714b966b6114538ab3f12d448179f203
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-04-24 08:48:13 -0300

    exemplo JPA finalizado

--- aula-spring-jpa/src/main/java/Repository/StartApp.java
+++ aula-spring-jpa/src/main/java/Repository/StartApp.java
@@ -1,4 +1,27 @@
 package Repository;
 
-public class StartApp {
+import Model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+@Component
+public class StartApp implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User user = new User();
+        user.setName("Jefferson");
+        user.setUsername("Jeff");
+        user.setPassword("Dio");
+
+        userRepository.save(user);
+
+        for (User u : userRepository.findAll()){
+            System.out.println(u);
+        }
+    }
 }
