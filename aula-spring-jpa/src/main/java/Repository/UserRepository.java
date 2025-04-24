ebc20713d50629e521c4e57a3f07ac610e5278dd
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-04-24 08:16:22 -0300

    inicializando a classe start app

--- aula-spring-jpa/src/main/java/Repository/UserRepository.java
+++ aula-spring-jpa/src/main/java/Repository/UserRepository.java
@@ -1,4 +1,7 @@
 package Repository;
 
-public class UserRepository {
+import Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
 }
