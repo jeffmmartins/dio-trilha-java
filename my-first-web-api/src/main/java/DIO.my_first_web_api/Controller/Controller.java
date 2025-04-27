b0daf57d13fb5a085045f8aa6168447576cbf294
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-04-26 21:39:41 -0300

    exemplo api finalizado

--- my-first-web-api/src/main/java/DIO.my_first_web_api/Controller/Controller.java
+++ my-first-web-api/src/main/java/DIO.my_first_web_api/Controller/Controller.java
@@ -1,4 +1,12 @@
 package DIO.my_first_web_api.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
+    @GetMapping
+    public String welcomw(){
+        return "Welcome to My Spring Boot Web API";
+    }
 }
