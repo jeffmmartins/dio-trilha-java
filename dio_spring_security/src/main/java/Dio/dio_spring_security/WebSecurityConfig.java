8d8d1fdcbca41c8ec844f1795ec130499e2b4243
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-04-30 17:49:19 -0300

    fix: cÃÃódigo corrigido

--- dio_spring_security/src/main/java/Dio/dio_spring_security/WebSecurityConfig.java
+++ dio_spring_security/src/main/java/Dio/dio_spring_security/WebSecurityConfig.java
@@ -1,4 +1,53 @@
 package Dio.dio_spring_security;
 
-public class WebSecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration // denominamos a classe de configuração
+@EnableGlobalMethodSecurity(prePostEnabled = true)// permite usar anotações como @PreAuthorize nos métodos para controlar quem pode acessá-los (exemplo: só administradores).
+public class WebSecurityConfig  {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults()); // para login via HTTP Basic
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User
+                .withUsername("Jeff")
+                .password("user123")
+                .roles("USERS")
+                .build();
+
+        UserDetails admin = User
+                .withUsername("admin")
+                .password("master123")
+                .roles("MANAGERS")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance(); // Só para testes. Em produção, use BCryptPasswordEncoder!
+    }
+
 }
