c0055ac2dc2001062bcda865d02b724d2ed142a3
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-05-01 11:32:10 -0300

    classe userrepository finalizada

---
+++ dio-spring-security-jwt/dio-spring-security-jwt/src/main/java/DIO/dio_spring_security_jwt/reposiory/UserRepository.java
@@ -0,0 +1,15 @@
+package DIO.dio_spring_security_jwt.reposiory;
+
+import DIO.dio_spring_security_jwt.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+    @Query("SELECT e FROM User e JOIN FETCH e.roles WHERE e.username= (:username)")
+    public User findByUsername(@Param("username") String username);
+
+    boolean existsByUsername(String username);
+
+    //dois metodos vão no banco de dados e ttrazzer as respectivas informações.
+}
