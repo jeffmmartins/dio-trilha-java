268d48014160cc0bf4f08b1b29a98df84ee07fde
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-05-01 17:15:55 -0300

    classe jwtcreator finalizada

--- dio-spring-security-jwt/dio-spring-security-jwt/src/main/java/DIO/dio_spring_security_jwt/security/JWTObject.java
+++ dio-spring-security-jwt/dio-spring-security-jwt/src/main/java/DIO/dio_spring_security_jwt/security/JWTObject.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
-public class JTWObject {
+public class JWTObject {
     private String subject; //nome do usuario
     private Date issuedAt; //data de criação do token
     private Date expiration; // data de expiração do token
