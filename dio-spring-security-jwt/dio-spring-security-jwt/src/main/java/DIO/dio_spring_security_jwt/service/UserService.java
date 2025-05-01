1ac9c0e54d56f9343195812ff7eebc100055d0f7
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-05-01 11:34:37 -0300

    classe userservice finalizada

--- dio-spring-security-jwt/dio-spring-security-jwt/src/main/java/DIO/dio_spring_security_jwt/service/UserService.java
+++ dio-spring-security-jwt/dio-spring-security-jwt/src/main/java/DIO/dio_spring_security_jwt/service/UserService.java
@@ -1,4 +1,22 @@
 package DIO.dio_spring_security_jwt.service;
 
+import DIO.dio_spring_security_jwt.model.User;
+import DIO.dio_spring_security_jwt.reposiory.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+    @Autowired
+    private PasswordEncoder encoder;
+    public void createUser(User user){
+        String pass = user.getPassword();
+        //criptografando antes de salvar no banco
+        user.setPassword(encoder.encode(pass));
+        repository.save(user);
+    }
 }
