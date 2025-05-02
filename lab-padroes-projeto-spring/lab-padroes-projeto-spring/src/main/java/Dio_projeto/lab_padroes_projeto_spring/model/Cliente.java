9ed1153886c5030d4aaeddbe2434aacb064bdca8
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-05-02 17:14:59 -0300

    classe endereco e cliente finalizado

--- lab-padroes-projeto-spring/lab-padroes-projeto-spring/src/main/java/Dio_projeto/lab_padroes_projeto_spring/model/Cliente.java
+++ lab-padroes-projeto-spring/lab-padroes-projeto-spring/src/main/java/Dio_projeto/lab_padroes_projeto_spring/model/Cliente.java
@@ -11,4 +11,28 @@ public class Cliente {
     private String nome;
     @ManyToOne // muitios apara um o endereç pode está para muitos
     private String endereco;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
 }
