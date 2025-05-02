21400a0600d51142b243d73ec9726bbae949e8b2
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-05-02 20:51:35 -0300

    classe clienteserviceimpl

---
+++ lab-padroes-projeto-spring/lab-padroes-projeto-spring/src/main/java/Dio_projeto/lab_padroes_projeto_spring/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,31 @@
+package Dio_projeto.lab_padroes_projeto_spring.service.impl;
+
+import Dio_projeto.lab_padroes_projeto_spring.model.Cliente;
+import Dio_projeto.lab_padroes_projeto_spring.service.ClienteService;
+
+public class ClienteServiceImpl implements ClienteService {
+    @Override
+    public Iterable<Cliente> buscarTodos() {
+        return null;
+    }
+
+    @Override
+    public Cliente buscarPorId(Long id) {
+        return null;
+    }
+
+    @Override
+    public void inserir(Cliente cliente) {
+
+    }
+
+    @Override
+    public void atualizar(Long id, Cliente cliente) {
+
+    }
+
+    @Override
+    public void deletar(Long id) {
+
+    }
+}
