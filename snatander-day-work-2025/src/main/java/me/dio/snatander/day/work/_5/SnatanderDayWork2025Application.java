0e412d78def4a289e414b1a04d41cb9afba92417
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-05-04 10:25:11 -0300

    inicializando o projeto de desafio DIO

---
+++ snatander-day-work-2025/src/main/java/me/dio/snatander/day/work/_5/SnatanderDayWork2025Application.java
@@ -0,0 +1,13 @@
+package me.dio.snatander.day.work._5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnatanderDayWork2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnatanderDayWork2025Application.class, args);
+	}
+
+}
---
+++ snatander-day-work-2025/src/test/java/me/dio/snatander/day/work/_5/SnatanderDayWork2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.snatander.day.work._5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnatanderDayWork2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
