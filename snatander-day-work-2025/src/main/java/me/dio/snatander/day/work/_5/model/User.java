5744fc275bcc43f444eb2522e985a879cfaaa622
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-05-04 12:57:29 -0300

    adicinando o nome da tabela

--- snatander-day-work-2025/src/main/java/me/dio/snatander/day/work/_5/model/User.java
+++ snatander-day-work-2025/src/main/java/me/dio/snatander/day/work/_5/model/User.java
@@ -1,13 +1,12 @@
 package me.dio.snatander.day.work._5.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.util.List;
 
 @Entity
+
+@Table(name = "tab_user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
