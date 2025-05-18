4d786515930e1e625fb01c103f4dbed78adc0abd
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-05-18 11:46:25 -0300

    implementando metodos no userserviceimpl

--- snatander-day-work-2025/src/main/java/me/dio/snatander/day/work/_5/service/UserService.java
+++ snatander-day-work-2025/src/main/java/me/dio/snatander/day/work/_5/service/UserService.java
@@ -1,4 +1,9 @@
 package me.dio.snatander.day.work._5.service;
 
+import me.dio.snatander.day.work._5.model.User;
+
 public interface UserService {
+    User findById(Long id);
+
+    User create(User userToCreate);
 }
--- snatander-day-work-2025/src/main/java/me/dio/snatander/day/work/_5/service/impl/UserServiceImpl.java
+++ snatander-day-work-2025/src/main/java/me/dio/snatander/day/work/_5/service/impl/UserServiceImpl.java
@@ -1,6 +1,17 @@
 package me.dio.snatander.day.work._5.service.impl;
 
+import me.dio.snatander.day.work._5.model.User;
 import me.dio.snatander.day.work._5.service.UserService;
 
 public class UserServiceImpl extends UserService {
+
+    @Override
+    public User findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        return null;
+    }
 }
