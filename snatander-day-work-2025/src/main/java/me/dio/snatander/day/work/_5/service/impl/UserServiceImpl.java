d6be571276e791861bf11d7854682d9b89bb2336
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-05-18 12:05:32 -0300

    lançando exceções

--- snatander-day-work-2025/src/main/java/me/dio/snatander/day/work/_5/service/impl/UserServiceImpl.java
+++ snatander-day-work-2025/src/main/java/me/dio/snatander/day/work/_5/service/impl/UserServiceImpl.java
@@ -1,17 +1,33 @@
 package me.dio.snatander.day.work._5.service.impl;
 
 import me.dio.snatander.day.work._5.model.User;
+import me.dio.snatander.day.work._5.repository.UserRepository;
 import me.dio.snatander.day.work._5.service.UserService;
+import org.springframework.stereotype.Service;
 
-public class UserServiceImpl extends UserService {
+import java.util.NoSuchElementException;
+
+@Service //indicando que vai ser nossa camada de negocio
+public class UserServiceImpl implements UserService {
+
+    // para criar esse serviço , espring entená que é necessario injetar o userRepository;
+    private final UserRepository userRepository;// interface de acesso a dados
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public User findById(Long id) {
-        return null;
+        //caso não encontre o id ele lança um exceção.
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
     }
 
     @Override
     public User create(User userToCreate) {
-        return null;
+        // usuário foro  diferente de null e identificra que o usuário já existe, lança uma exceção;
+        if(userToCreate.getId() != null &&  userRepository.existsById(userToCreate.getId())){
+            throw new IllegalArgumentException("This User Id already exxist.");
+        }
     }
 }
