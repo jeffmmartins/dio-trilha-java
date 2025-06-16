36d2c4d98043e470fab7956cbae1fc0cdd9e4c60
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-06-16 15:05:14 -0300

    Feat: exemplo finalizado

--- controle-de-candidato/src/Main.java
+++ controle-de-candidato/src/Main.java
@@ -1,10 +1,40 @@
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-       selecaoCandidatos();
+        String [] candidatos = {"Jeff", "Martins", "Mendes", "Vera", "lucia"};
+        for(String candidato : candidatos) {
+            entrandoEmContato(candidato);
+        }
+
+    }
+
+    static void entrandoEmContato(String candidato){
+        int tentativasRealizada = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando){
+                tentativasRealizada++;
+            }else{
+                System.out.println("Conatdo realizado com sucesso. ");
+            }
+        }while (continuarTentando && tentativasRealizada < 3);{
+            if (atendeu){
+                System.out.println("Conseguimos contato com: " + candidato + " na " +tentativasRealizada);
+            }else{
+                System.out.println("Não conseguimos contato com " + candidato + "número maximo de tentativas " + tentativasRealizada);
+            }
+        }
+    }
+
+    static boolean atender(){
+        return new Random().nextInt(3)==1;
     }
 
     static void imprimirSelecionados() {
