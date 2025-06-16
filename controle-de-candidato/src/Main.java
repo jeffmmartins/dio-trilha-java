23203b488b7c9f8858b686fe7323cbdd86f20cca
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-06-16 14:07:20 -0300

    desafio trilha dio java

---
+++ controle-de-candidato/src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+       analisarCandidato(3000.0);
+    }
+
+    static void analisarCandidato(double salarioPretendido){
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido) {
+            System.out.println("Ligar para o candidato");
+        } else if (salarioBase == salarioPretendido) {
+            System.out.println("ligar para o canditado com uma contra proposta");
+        }else{
+            System.out.println("Aguuardando resultado dos demais candidatos");
+        }
+    }
+}
--- lab-padroes-projeto-spring/lab-padroes-projeto-spring/src/main/java/Dio_projeto/lab_padroes_projeto_spring/service/impl/ClienteServiceImpl.java
+++ lab-padroes-projeto-spring/lab-padroes-projeto-spring/src/main/java/Dio_projeto/lab_padroes_projeto_spring/service/impl/ClienteServiceImpl.java
@@ -1,22 +1,47 @@
 package Dio_projeto.lab_padroes_projeto_spring.service.impl;
 
 import Dio_projeto.lab_padroes_projeto_spring.model.Cliente;
+import Dio_projeto.lab_padroes_projeto_spring.model.ClienteRepository;
+import Dio_projeto.lab_padroes_projeto_spring.model.Endereco;
+import Dio_projeto.lab_padroes_projeto_spring.model.EnderecoRepository;
 import Dio_projeto.lab_padroes_projeto_spring.service.ClienteService;
+import Dio_projeto.lab_padroes_projeto_spring.service.ViaCepSevice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
+//exemplo de facade
 public class ClienteServiceImpl implements ClienteService {
-    @Override
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+    @Autowired
+    private ViaCepSevice viaCepSevice;
+
+    @Override // buscar cliente no repository
     public Iterable<Cliente> buscarTodos() {
-        return null;
+        return clienteRepository.findAll();
     }
 
     @Override
     public Cliente buscarPorId(Long id) {
-        return null;
+        Optional<Cliente> cliente = clienteRepository.findById(id);
+        return cliente.get();
     }
 
     @Override
     public void inserir(Cliente cliente) {
-
+        String cep = cliente.getEndereco().getCep();
+        Endereco endereco = enderecoRepository.findById(cep).orElse(() -> {
+            Endereco novoEndereco = viaCepSevice.consultaCep(cep);
+            enderecoRepository.save(novoEndereco);
+            return null;
+        });
+        cliente.setEndereco(endereco);
     }
 
     @Override
