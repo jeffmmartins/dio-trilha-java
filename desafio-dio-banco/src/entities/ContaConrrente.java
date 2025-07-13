69801703ce12c0ea612a6b8917e0a9425fdda47e
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-07-12 22:45:26 -0300

    Feat: inicializando as classes ContaCorrente, ContaPoupanca, Banco, Cliente

---
+++ desafio-dio-banco/src/entities/ContaConrrente.java
@@ -0,0 +1,21 @@
+package entities;
+
+public class ContaConrrente {
+
+    private int agencia;
+    private int contaCorrente;
+    private int saldo;
+
+    public void sacar(){
+
+    }
+
+    public void despositar(){
+
+    }
+
+    public void transferir(){
+
+    }
+
+}
---
+++ desafio-dio-banco/src/entities/ContaPoupanca.java
@@ -0,0 +1,20 @@
+package entities;
+
+public class ContaPoupanca {
+
+    private int agencia;
+    private int contaPoupança;
+    private int saldo;
+
+    public void sacar(){
+
+    }
+
+    public void despositar(){
+
+    }
+
+    public void transferir(){
+
+    }
+}
